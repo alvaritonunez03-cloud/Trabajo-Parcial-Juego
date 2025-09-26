7c974195ed910afee1b8b522a3dc83084e4a6e41
alvaritonunez03-cloud <alvaritonunez03@gmail.com>
2025-09-25 21:44:27 -0500

    Update Mundos.h
    Ahora con musica

--- Mundos.h
+++ Mundos.h
@@ -1,11 +1,15 @@
+
 #pragma once
 #include "Pantalla.h"
 #include "Personaje.h"
+#include "Musica.h"
 
 class Mundo {
 private:
 	Pantalla* pantalla;
 	Personaje jugador;
+	Pantalla mundo[3][3];
+	int pantallaX, pantallaY;
 public:
 	Mundo();
 	~Mundo();
@@ -16,39 +20,73 @@ Mundo::Mundo() {};
 Mundo::~Mundo() {};
 void Mundo::mundo1() {
 	int tecla;
-	bool p1, p2, p3, p4;
+	bool p1, p2, p3, p4, mundo1 = true;
 	p1 = true; p2 = false; p3 = false; p4 = false;
-	while (p1)
-	{
-	pantalla->dibujarPantalla1();
-	jugador.dibujar();
-		if (_kbhit())
-		{
-			tecla = _getch();
-			jugador.borrar();
-			jugador.movimiento(tecla);
-			jugador.dibujar();
-		}
-		pantalla->cambiarPantalla(jugador.getX(),jugador.getY(),p1,p2);
-		_sleep(100);
-	}
-	jugador.borrar();
-	jugador.setX(100);//Para posicionar al jugadro al otro lado
-	jugador.setY(10);//Creo que es mejor hacer una clase en el jugador xd
-	while (p2)
-	{
-		pantalla->dibujarPantalla2();
 
-		jugador.dibujar();
-		if (_kbhit())
+	Musica musica1("43. Castle Funk (DELTARUNE Chapter 34 Soundtrack) - Toby Fox.wav");
+	musica1.reproducir(true);
+
+	mundo[2][2].dibujarPantalla1();
+	while (mundo1)
+	{
+		while (p1)
 		{
-			tecla = _getch();
-			jugador.borrar();
-			jugador.movimiento(tecla);
 			jugador.dibujar();
+			if (_kbhit())
+			{
+				tecla = _getch();
+				jugador.borrar();
+				jugador.movimiento(tecla);
+				jugador.dibujar();
+			}
+			pantalla->cambiarPantalla1(jugador.getX(), jugador.getY(), p1, p2, p3);
+			_sleep(30);
 		}
 
-		_sleep(100);
+		if (p2) {
+			jugador.borrar();
+			jugador.setX(100);//Para posicionar al jugadro al otro lado
+			jugador.setY(10);//Creo que es mejor hacer una clase en el jugador xd
+		}
+		if (p3) {
+			jugador.borrar();
+			jugador.setX(10);//Para posicionar al jugadro al otro lado
+			jugador.setY(10);//Creo que es mejor hacer una clase en el jugador xd
+		}
+
+		while (p2)
+		{
+			jugador.dibujar();
+			if (_kbhit())
+			{
+				tecla = _getch();
+				jugador.borrar();
+				jugador.movimiento(tecla);
+				jugador.dibujar();
+			}
+
+			pantalla->cambiarPantalla21(jugador.getX(), jugador.getY(), p1, p2);
+			_sleep(30);
+		}
+
+		if (p1) {
+			jugador.borrar();
+			jugador.setX(10);//Para posicionar al jugadro al otro lado
+			jugador.setY(10);//Creo que es mejor hacer una clase en el jugador xd
+		}
+
+
+		while (p3)
+		{
+			jugador.dibujar();
+			if (_kbhit())
+			{
+				tecla = _getch();
+				jugador.borrar();
+				jugador.movimiento(tecla);
+				jugador.dibujar();
+			}
+		}
+
 	}
-	
 }
